15f5959b7dd7ac2507b5b52b42b37fd2941bf712
Jean Melo <31326375+jeanmamelo@users.noreply.github.com>
2020-08-24 15:42:14 -0300

    Tela de Loading
    Algumas alterações na tela de carregamento
    
    Co-Authored-By: Eduardo Fiorito <45640440+eduardofiorito@users.noreply.github.com>

--- ProjetoInterdisciplinar/screens/LoadingScreen.js
+++ ProjetoInterdisciplinar/screens/LoadingScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
+import { Text, View, StyleSheet, ActivityIndicator, Image } from 'react-native';
 
 export default class LoadingScreen extends React.Component {
     render() {
         return (
-            <View>
-                <Text style={Styles.container}>Carregando...</Text>
+            <View style={styles.container}>
+
+                <Image
+                    source={require('../assets/cdpc-circular.png')}
+                    style={styles.logo}
+                ></Image>
+
+                <Text style={styles.loading}>Carregando...</Text>
                 <ActivityIndicator size='large'></ActivityIndicator>
+                
             </View>
         )
     }
@@ -15,7 +22,16 @@ export default class LoadingScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: "center",
         alignItems: "center"
+    },
+    loading: {
+        fontSize: 28,
+        marginBottom: 10
+    },
+    logo: {
+        alignSelf: "center",
+        height: 280,
+        width: 280
     }
 })
