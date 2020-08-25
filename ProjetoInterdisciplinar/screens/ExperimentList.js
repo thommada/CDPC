95f60f1a0b4166fb607bb159a7f3a33890e521f2
Thomas Adam da Costa <thomas.costa@alunos.ifsuldeminas.edu.br>
2020-08-25 14:54:17 -0300

    Lista de Experimentos
    Atualização da estrutura de View da tela
    Style

--- ProjetoInterdisciplinar/screens/ExperimentList.js
+++ ProjetoInterdisciplinar/screens/ExperimentList.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Style from '../styles/ExperimentListStyle'
 
 class ExperimentList extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Lista de Experimentos</Text>
+            <View style={Style.container}>
+                <View style={Style.boxtitle}>
+                    <Text style={Style.title}>Lista de Experimentos</Text>
+                </View>
+                <View style={Style.box}>
+                    <Text style={Style.subtitle}>Corpo</Text>
+                    
+                </View>
             </View>
         )
     }
--- ProjetoInterdisciplinar/styles/ExperimentListStyle.js
+++ ProjetoInterdisciplinar/styles/ExperimentListStyle.js
@@ -1,12 +1,35 @@
 import { StyleSheet } from 'react-native'
 
+
 export default StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
+        backgroundColor: "whitesmoke",
+    },
+    boxtitle: {
+        flex: 1,
+        padding: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        
+    },
+    box: {
+        flex: 6,
+        padding: 20,
+        justifyContent: "space-between",
+        alignItems: "flex-start"
     },
     title: {
-
+        fontSize: 32,
+        fontWeight: "200",
+    },
+    subtitle: {
+        fontSize: 24,
+        fontWeight: "100",
+    },
+    itemtext: {
+        fontSize: 18,
+        fontWeight: "normal",
     }
 })
