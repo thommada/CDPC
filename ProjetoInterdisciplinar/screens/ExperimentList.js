737aa3f651954ced95715a23bf90e7ac2370be70
Thomas Adam da Costa <thomas.costa@alunos.ifsuldeminas.edu.br>
2020-08-25 13:54:48 -0300

    Criação ExperimentList
    Adicionando:
    * ExperimentList.js
    * experimentListStyle.js

---
+++ ProjetoInterdisciplinar/screens/ExperimentList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+class ExperimentList extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Lista de Experimentos</Text>
+            </View>
+        )
+    }
+}
+export default ExperimentList
