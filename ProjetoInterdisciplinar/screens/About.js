ebf9daa7d0bdfa0ec67a5880165fcf1618e594ae
henriquevm <henriqueveigamoreira@gmail.com>
2020-08-26 21:10:28 -0300

    Tela sobre e logo para carregar app
    Texto e imagem na tela sobre (About.js) e logo para carregar app

--- ProjetoInterdisciplinar/screens/About.js
+++ ProjetoInterdisciplinar/screens/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View,  Image } from 'react-native'
+import { Text, View, Image, StyleSheet } from 'react-native'
 
 import DefaultStyle from '../styles/DefaultStyle'
 
@@ -7,9 +7,21 @@ class About extends React.Component {
     render() {
         return (
             <View style={DefaultStyle.containerCenter}>
-                <Text>Sobre o CDPC</Text>
+                <Text style={styles.text}>O CDPC tem por objetivo a difusão e disseminação da ciência e vai atuar na formação e acompanhamento de professores, principalmente da rede pública de ensino de Poços de Caldas, contribuindo com a melhoria da qualidade da educação básica nas áreas de física, química e biologia. Serão realizadas exposições interativas que farão dos visitantes protagonistas dos experimentos. Já a “Experimentoteca” vai oferecer kits portáteis de experimentos na área de ciências da natureza, que serão emprestados aos professores da rede básica de ensino para que possam desenvolver atividades com os alunos em sala de aula.</Text>
+                <Image
+                    source={require('../assets/img/cdpc-circular.png')}
+                    style={DefaultStyle.logo}
+                ></Image>
             </View>
         )
     }
 }
 export default About
+
+const styles = StyleSheet.create({
+    text: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+});
