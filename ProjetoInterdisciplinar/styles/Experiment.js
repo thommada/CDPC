20f9c7b39c96b5befe778964231d48193b4dbc9b
eduardofiorito <efiorito@live.com>
2020-08-27 23:09:31 -0300

    Melhoria nas telas About.js e ExperimentProfile.js
    -Incrementei o conteúdo da tela Sobre o CDPC e padronizei o estilo de acordo com o restante das telas.
    -Alterei a estilização da tela do Perfil do Experimento, para padronizar com as outras telas.

--- ProjetoInterdisciplinar/styles/Experiment.js
+++ ProjetoInterdisciplinar/styles/Experiment.js
@@ -2,28 +2,33 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
 	title: {
-		marginTop: 50,
-		textAlign: 'center',
+		marginHorizontal: 15,
+		marginTop: 25,
+		marginBottom: 20,
 		fontSize: 30,
 		fontWeight: 'bold',
-		marginBottom: 20,
-		textDecorationLine: 'underline',
-		color: '#1C1C1C'
+		color: '#111111'
 	},
 	subtitle: {
 		fontWeight: 'bold',
 		fontSize: 22,
-		marginTop: 20,
-		color: '#1C1C1C'
+		color: '#1C1C1C',
+		marginBottom: 10
 	},
 	text: {
-		fontSize: 16,
+		marginBottom: 25,
+		fontSize: 18,
 		textAlign: 'justify',
-		color: '#363636'
+		color: '#333333',
+		marginHorizontal: 15,
+		lineHeight: 30
 	},
 	textPDF: {
-		fontSize: 16,
+		marginTop: 10,
+		fontSize: 22,
 		textAlign: 'justify',
-		color: '#0000CD'
+		color: '#0000CD',
+		marginHorizontal: 15,
+		marginBottom: 20
 	}
 })
