ae6cdc55ced90c85ec7874412e73e5c6df48a45e
Thomas Adam da Costa <thomas.costa@alunos.ifsuldeminas.edu.br>
2020-08-30 20:25:19 -0300

    ExperimentList
    Alteração na lista
    Alteração no Estilo da Tela Experiment List
    
    Co-Authored-By: Henrique <40326185+henriquevm@users.noreply.github.com>

--- ProjetoInterdisciplinar/styles/ExperimentListStyle.js
+++ ProjetoInterdisciplinar/styles/ExperimentListStyle.js
@@ -3,39 +3,26 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
     boxtitle: {
         flex: 1,
-        padding: 20,
         justifyContent: "center",
         alignItems: "center",
-    },
-    box: {
-        flex: 5,
-        justifyContent: "center",
-        alignItems: "stretch",
     },
     title: {
         fontSize: 32,
         fontWeight: "200",
     },
-    subtitle: {
-        fontSize: 24,
-        fontWeight: "100",
-        paddingLeft: 20,
-    },
-    text: {
-        fontSize: 16,
-        color: "black",
-        fontWeight: "normal",
-        paddingLeft: 50,
+    box: {
+        flex: 6,
+        justifyContent: "center",
+        alignItems: "center",
     },
     header: {
-        padding: 20,
+        fontSize: 24,
+        fontWeight: "100",
     },
-    content: {
-        padding: 10,
-    },
-    simbol: {
-        alignSelf: "center",
-        height: 20,
-        width: 20,
+    text: {
+        fontSize: 20,
+        color: "black",
+        fontWeight: "normal",
+        paddingLeft: 15,
     },
 })
