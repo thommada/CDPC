ada8541fcce3fdb2dfc961c70f7349849787bd63
henriquevm <henriqueveigamoreira@gmail.com>
2020-08-30 20:51:38 -0300

    Navegação para de lista de experimenos para perfil de experimento
    Navegação para de lista de experimenos para perfil de experimento
    
    Co-Authored-By: Thomas Adam da Costa <49987827+gordodemoniaco@users.noreply.github.com>

--- ProjetoInterdisciplinar/screens/ExperimentList.js
+++ ProjetoInterdisciplinar/screens/ExperimentList.js
@@ -4,20 +4,20 @@ import Style from '../styles/ExperimentListStyle';
 import DefaultStyle from '../styles/DefaultStyle';
 
 const physics = [
-    {id: 101, title:'Disco Flutuante'},
-    {id: 102, title:'Conservação de Energia'},
-    {id: 103, title:'Energia Cinética'},
+    { id: 101, title: 'Disco Flutuantes',  screen: 'FloatingDiskScreen'},
+    { id: 102, title: 'Energia de Movimento',  screen: 'MotionEnergyScreen' },
+    { id: 103, title: 'Conservação de Energia',  screen: 'EnergyConservationIScreen' },
 ]
 const chemistry = [
-    {id: 201, title:'Ácido-Base'},
-    {id: 202, title:'Soluçao Tampão'},
+    { id: 201, title: 'Ácido-Base',  screen: 'FloatingDiskScreen' },
+    { id: 202, title: 'Soluçao Tampão',  screen: 'FloatingDiskScreen' },
 ]
 const biology = [
-    {id: 301, title:'Deterioração do Plástico'},
-    {id: 302, title:'Digestão'},
+    { id: 301, title: 'Deterioração do Plástico',  screen: 'FloatingDiskScreen' },
+    { id: 302, title: 'Digestão',  screen: 'FloatingDiskScreen' },
 ]
 const microbiology = [
-    {id: 401, title:'Bacterias'},
+    { id: 401, title: 'Bacterias', screen: 'FloatingDiskScreen'},
 ]
 
 export default class ExperimentList extends React.Component {
@@ -30,31 +30,34 @@ export default class ExperimentList extends React.Component {
                 </View>
                 <SafeAreaView style={Style.box}>
                     <SectionList
-                        sections = {[
-                            {title: "Física", data: physics},
-                            {title: "Química", data: chemistry},
-                            {title: "Biologia", data: biology},
-                            {title: "Microbiologia", data: microbiology},
+                        sections={[
+                            { title: "Física", data: physics },
+                            { title: "Química", data: chemistry },
+                            { title: "Biologia", data: biology },
+                            { title: "Microbiologia", data: microbiology },
                         ]}
-                        renderItem= {( {item}) =>(
+                        renderItem={({ item }) => (
                             <View>
-                                <Text style={Style.text}>
-                                    {item.title}
-                                </Text>
+                                <TouchableOpacity onPress={() => this.props.navigation.navigate(item.screen)}
+                                >
+                                    <Text style={Style.text}>
+                                        {item.title}
+                                    </Text>
+                                </TouchableOpacity>
                             </View>
                         )}
-                        renderSectionHeader= {({section}) => (
+                        renderSectionHeader={({ section }) => (
                             <View>
                                 <Text style={Style.header}>
                                     {section.title}
                                 </Text>
                             </View>
                         )}
-                        keyExtractor={ (item) => item.id}
+                        keyExtractor={(item) => item.id}
                     />
                 </SafeAreaView>
             </View>
-            
+
         )
     }
 }
@@ -62,7 +65,7 @@ export default class ExperimentList extends React.Component {
 const CONTENT = [
     {
         title: 'Física',
-        data: [, 'Energia de Movimento', 'Conservação de Energia'],
+        data: ['Energia de Movimento', 'Conservação de Energia'],
     },
     {
         title: 'Química',
