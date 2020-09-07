ed61905c160d488d72e452c1a441edb8e4b12baf
eduardofiorito <efiorito@live.com>
2020-09-07 15:40:08 -0300

    Update config.js
    Adicionei o firestore no config.js do firebase.

--- ProjetoInterdisciplinar/components/firebase/config.js
+++ ProjetoInterdisciplinar/components/firebase/config.js
@@ -1,26 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import 'firebase/firestore'
 
 // Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCOVPPTpBHjJoafmFBoNh3WwB9bxnHPd4A",
-    authDomain: "cdpc-326f1.firebaseapp.com",
-    databaseURL: "https://cdpc-326f1.firebaseio.com",
-    projectId: "cdpc-326f1",
-    storageBucket: "cdpc-326f1.appspot.com",
-    messagingSenderId: "368958949032",
-    appId: "1:368958949032:web:ef1606063f02350b1de49d"
-  }
+const firebaseConfig = {
+	apiKey: "AIzaSyCOVPPTpBHjJoafmFBoNh3WwB9bxnHPd4A",
+	authDomain: "cdpc-326f1.firebaseapp.com",
+	databaseURL: "https://cdpc-326f1.firebaseio.com",
+	projectId: "cdpc-326f1",
+	storageBucket: "cdpc-326f1.appspot.com",
+	messagingSenderId: "368958949032",
+	appId: "1:368958949032:web:ef1606063f02350b1de49d"
+}
 
 // Initialize Firebase
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+	firebase.initializeApp(firebaseConfig)
+	firebase.firestore()
 }
-export { firebase };
-
-
-
-
-
-  
+export default firebase
