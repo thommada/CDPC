a64f090bfe51815c8d7a5d88cae2776d7a61220a
Thomas Adam da Costa <thomas.costa@alunos.ifsuldeminas.edu.br>
2020-09-08 13:59:58 -0300

    Revert "ExperimentList Att"
    This reverts commit ab3a7a001a2b94ca790c4e7a86002f5b2b8c7e33.

--- ProjetoInterdisciplinar/screens/ExperimentList.js
+++ ProjetoInterdisciplinar/screens/ExperimentList.js
@@ -3,49 +3,44 @@ import { Text, View, SafeAreaView, SectionList, TouchableOpacity } from 'react-n
 import Style from '../styles/ExperimentListStyle';
 import DefaultStyle from '../styles/DefaultStyle';
 
-const experimentos = [
-    {   
-        titulo: "Física",
-        data: [
-        { id: 101, title: 'Disco Flutuantes',  screen: 'FloatingDiskScreen'},
-        { id: 102, title: 'Energia de Movimento',  screen: 'MotionEnergyScreen' },
-        { id: 103, title: 'Conservação de Energia',  screen: 'EnergyConservationIScreen' },
-    ]},
-    {   
-        titulo: "Química",
-        data: [
-        { id: 201, title: 'Ácido-Base',  screen: 'GenericExperiment' },
-        { id: 202, title: 'Soluçao Tampão',  screen: 'GenericExperiment' },
-    ]},
-    {
-        titulo: "Biologia",
-        data : [
-        { id: 301, title: 'Deterioração do Plástico',  screen: 'GenericExperiment' },
-        { id: 302, title: 'Digestão',  screen: 'GenericExperiment' },
-    ]},
-    {
-        titulo: "Microbiologia",
-        data : [
-        { id: 401, title: 'Bacterias', screen: 'GenericExperiment'},
-    ]},
-    {
-        titulo: "Disciplina",
-        data : [
-        { id: 501, title: 'Nome do Experimento', screen: 'GenericExperiment'},
-        { id: 601, title: 'Nome do Experimento (só que grande como exemplo)', screen: 'GenericExperiment'},
-    ]},
+const physics = [
+    { id: 101, title: 'Disco Flutuantes',  screen: 'FloatingDiskScreen'},
+    { id: 102, title: 'Energia de Movimento',  screen: 'MotionEnergyScreen' },
+    { id: 103, title: 'Conservação de Energia',  screen: 'EnergyConservationIScreen' },
 ]
+const chemistry = [
+    { id: 201, title: 'Ácido-Base',  screen: 'GenericExperiment' },
+    { id: 202, title: 'Soluçao Tampão',  screen: 'GenericExperiment' },
+]
+const biology = [
+    { id: 301, title: 'Deterioração do Plástico',  screen: 'GenericExperiment' },
+    { id: 302, title: 'Digestão',  screen: 'GenericExperiment' },
+]
+const microbiology = [
+    { id: 401, title: 'Bacterias', screen: 'GenericExperiment'},
+]
+const disciplina = [
+    { id: 501, title: 'Nome do Experimento', screen: 'GenericExperiment'},
+    { id: 601, title: 'Nome do Experimento (só que grande como exemplo)', screen: 'GenericExperiment'},
+]
+
 export default class ExperimentList extends React.Component {
     render() {
         return (
             <View style={DefaultStyle.container}>
                 <View style={Style.boxtitle}>
                     <Text style={Style.title}>Lista de Experimentos</Text>
+
                 </View>
                 <SafeAreaView style={Style.box}>
                     <SectionList
-                        sections={experimentos}
-                        keyExtractor={(item, index ) => item + index}
+                        sections={[
+                            { title: "Física", data: physics },
+                            { title: "Química", data: chemistry },
+                            { title: "Biologia", data: biology },
+                            { title: "Microbiologia", data: microbiology },
+                            { title: "Disciplina", data: disciplina},
+                        ]}
                         renderItem={({ item }) => (
                             <View style={Style.boxtext}>
                                 <TouchableOpacity onPress={() => this.props.navigation.navigate(item.screen)}>
@@ -55,14 +50,14 @@ export default class ExperimentList extends React.Component {
                                 </TouchableOpacity>
                             </View>
                         )}
-                        renderSectionHeader={({ section: {titulo} }) => (
+                        renderSectionHeader={({ section }) => (
                             <View style={Style.boxheader}>
                                 <Text style={Style.header}>
-                                    {titulo}
+                                    {section.title}
                                 </Text>
                             </View>
                         )}
-                        
+                        keyExtractor={(item) => item.id}
                     />
                 </SafeAreaView>
             </View>
